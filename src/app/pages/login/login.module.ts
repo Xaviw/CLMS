668422183fbf2e623ad04110b9a661ab3852352d
XW <974485374@qq.com>
2021-08-10 18:03:41 +0800

    模块化

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,30 +1,12 @@
-import { IconsProviderModule } from '../../shared/icons-provider.module';
+import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzMessageModule } from 'ng-zorro-antd/message';
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 
 const route: Routes = [{ path: '', component: LoginComponent }];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(route),
-    FormsModule,
-    ReactiveFormsModule,
-    IconsProviderModule,
-    NzFormModule,
-    NzInputModule,
-    NzButtonModule,
-    NzCheckboxModule,
-  ],
+  imports: [SharedModule, RouterModule.forChild(route)],
   declarations: [LoginComponent],
 })
 export class LoginModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './login/login.component';
 import { SharedModule } from './../shared/shared.module';
 import { NgModule, Type } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
