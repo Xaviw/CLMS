7902ca89145b2704584bd48ecb7ddffd34877e07
XW <974485374@qq.com>
2021-09-26 18:00:51 +0800

    添加课程

--- src/app/pages/course-manage/course-manage.component.ts
+++ src/app/pages/course-manage/course-manage.component.ts
@@ -1,6 +1,11 @@
+import { CacheService } from './../../core/services/cache.service';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { CourseManageService } from './course-manage.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CourseCard } from '@app/shared/types/commonTypes';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { NzSelectComponent } from 'ng-zorro-antd/select';
 
 @Component({
   selector: 'course-manage',
@@ -11,10 +16,76 @@ import { CourseCard } from '@app/shared/types/commonTypes';
 export class CourseManageComponent implements OnInit {
   // 课程卡片信息
   courseCardData: CourseCard[] = [];
+  //
+  @ViewChild('teacherSearchEl') teacherSearchEl!: NzSelectComponent;
+  // 添加课程抽屉
+  addCourseDrawer = {
+    flag: true,
+    visible: false,
+    searchSubject: new Subject<string>(),
+    keyWord: null,
+    teacherList: <{ value: string; label: string }[]>[],
+    validateForm: this.fb.group({
+      name: [null, [Validators.required]],
+      teacher: [null, [Validators.required]],
+      isRequired: [true, [Validators.required]],
+      class: [null, [Validators.required]],
+      weekRange: [null, [Validators.required]],
+      description: [null, []],
+    }),
+    requiredChange: (e: Event) => {
+      if (e) {
+        this.addCourseDrawer.validateForm.addControl('class', new FormControl(null, Validators.required));
+      } else {
+        this.addCourseDrawer.validateForm.removeControl('class');
+      }
+    },
+    keyUp: (e: Event) => {
+      if (this.addCourseDrawer.flag) {
+        // this.addCourseDrawer.searchSubject.next();
+        this.teacherSearchEl.nzOnSearch.emit();
+      }
+      // if (keyword.trim())
+    },
+    selectOpen: (e: boolean) => {
+      if (e) this.addCourseDrawer.keyWord = null;
+    },
+    selectSearch: (keyWord: string) => {
+      console.log('keyWord: ', keyWord);
+    },
+    selectChange: (value: null | string) => {
+      if (value === null) return;
+      // const data = {};
+      // this.service.queryPersonRowNumber(data).subscribe(num = >{
+      // 	table.pageIndex = parseInt(num / table.pageSize + '') + 1;
+      // 	table.selectedRowIndex = num % table.pageSize;
+      // 	table.evtPageChange(false, true);
+      // });
+    },
+    open: () => {
+      this.addCourseDrawer.visible = true;
+    },
+    close: () => {
+      this.addCourseDrawer.visible = false;
+    },
+  };
 
-  constructor(private service: CourseManageService) {}
+  constructor(private service: CourseManageService, private fb: FormBuilder, public cache: CacheService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // 默认选中自己
+    const user = this.cache.userInfo;
+    this.addCourseDrawer.teacherList = [{ value: user.account, label: user.name + '-' + user.account }];
+    this.addCourseDrawer.validateForm.get('teacher')?.patchValue(user.account);
+    // 添加课程--搜索教师触发
+    this.addCourseDrawer.searchSubject.pipe(debounceTime(500), distinctUntilChanged()).subscribe((keyword) => {
+      console.log('keyword: ', keyword);
+      // const data = {};
+      // this.service.queryPersonList(data).subscribe((result) => {
+      //   this.list = result;
+      // });
+    });
+  }
 
   // 接收级联参数
   getConditions(param: any) {
