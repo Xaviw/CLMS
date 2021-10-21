a503e366e6b640ce3c33079e0c22d8de511f1fe3
XW <974485374@qq.com>
2021-10-21 17:58:54 +0800

    编辑座位表

--- src/app/pages/lab-detail/seating-chart/seating-chart.component.ts
+++ src/app/pages/lab-detail/seating-chart/seating-chart.component.ts
@@ -1,5 +1,6 @@
 import { LabManageService } from './../../lab-manage/lab-manage.service';
 import { Component, Input, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'seating-chart',
@@ -23,10 +24,66 @@ export class SeatingChartComponent implements OnInit {
 
   freeTimeRange: number[] = []; // 空闲时间段
   selectedTime: number = -1; // 选中的时间段
-  seatingChart: Array<Array<number>> = []; // 机房座位表
   originalChart: Array<Array<number>> = []; // 原始座位表
+  seatingChart: Array<Array<number>> = []; // 座位表，包含预约信息
+  chartCopy: Array<Array<number>> = []; // 修改座位表
   color = [null, 'rgba(24,144,255,0.8)', 'rgba(128,128,128,0.8)', 'rgba(255,77,79,0.8)']; // 座位对应颜色
   date = new Date(); // 空闲时段日期，默认今天
+
+  // 设置座位表
+  settingChart = {
+    visible: false,
+    title: '',
+    row: 1,
+    column: 1,
+    open: (title: string) => {
+      if (this.originalChart?.length) {
+        this.settingChart.row = this.originalChart?.length;
+        this.settingChart.column = this.originalChart?.[0]?.length;
+        this.chartCopy = _.cloneDeep(this.originalChart);
+      } else {
+        this.settingChart.row = 1;
+        this.settingChart.column = 1;
+        this.chartCopy = [[1]];
+      }
+      this.settingChart.title = title;
+      this.settingChart.visible = true;
+    },
+    chartChange: (flag: boolean) => {
+      console.log(this.chartCopy);
+      const row = this.settingChart.row,
+        column = this.settingChart.column;
+      const row2 = this.chartCopy.length,
+        column2 = this.chartCopy[0].length;
+      if (flag) {
+        // 行变动
+        let diff = row - row2;
+        if (diff > 0) {
+          while (diff--) {
+            this.chartCopy.push(Array(column).fill(1));
+          }
+        } else {
+          for (let i = diff; i < 0; i++) {
+            this.chartCopy.pop();
+          }
+        }
+      } else {
+        // 列变动
+        let diff = column - column2;
+        if (diff > 0) {
+          this.chartCopy = this.chartCopy.map((item) => {
+            return [...item, ...Array(diff).fill(1)];
+          });
+        } else {
+          this.chartCopy = this.chartCopy.map((item) => item.slice(0, column2 + diff));
+        }
+      }
+    },
+    handleOk: () => {},
+    cancel: () => {
+      this.settingChart.visible = false;
+    },
+  };
 
   constructor(private service: LabManageService) {}
 
--- src/app/shared/directives/empty.directive.ts
+++ src/app/shared/directives/empty.directive.ts
@@ -11,9 +11,9 @@ export class EmptyDirective {
   // 需要校验的值--变化时触发元素修改
   @Input()
   set emptyData(data: any[]) {
-    if (!data.length && !this.el.nativeElement.contains(this.node)) {
+    if (!data?.length && !this.el.nativeElement.contains(this.node)) {
       this.el.nativeElement.append(this.node);
-    } else if (data.length && this.el.nativeElement.contains(this.node)) {
+    } else if (data?.length && this.el.nativeElement.contains(this.node)) {
       this.el.nativeElement.removeChild(this.node);
     }
   }
